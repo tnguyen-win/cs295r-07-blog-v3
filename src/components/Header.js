eb64c3bce7b8416cda7ad0906da2fb8a10896a2f
tnguyen-win <129979622+tnguyen-win@users.noreply.github.com>
2024-05-13 16:54:40 -0700

    Fix

--- src/components/Header.js
+++
@@ -1,1 +0,0 @@
-export default function Header({ src, height }) { return <img className='ratio ratio-16x9 mb-4' src={src} height={height} />; }
--- src/pages/NoPage.js
+++
@@ -1,1 +0,0 @@
-export default function NoPage() { return <h3 className="display-3 fw-bold text-center mt-5">404 Page Error</h3>; }
